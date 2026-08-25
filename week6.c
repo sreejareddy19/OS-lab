2301c5065bfa3f0c5d54ede6941f211278bba1c8
sreejareddy19 <gorlasreejareddy@gmail.com>
2026-08-25 10:14:16 +0530

    Add files via upload

---
+++ week6.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+int main()
+{
+int pages[50], frames[10];
+int n, f;
+int i, j;
+int pageFaults = 0, pageHits = 0;
+int front = 0;
+int found;
+printf("Enter the number of pages: ");
+scanf("%d", &n);
+printf("Enter the page reference string:\n");
+for (i = 0; i < n; i++)
+scanf("%d", &pages[i]);
+printf("Enter the number of frames: ");
+scanf("%d", &f);
+
+for (i = 0; i < f; i++)
+frames[i] = -1;
+printf("\nPage\tFrames\t\tStatus\n");
+for (i = 0; i < n; i++)
+{
+found = 0;
+for (j = 0; j < f; j++)
+{
+if (frames[j] == pages[i])
+
+{
+found = 1;
+break;
+}
+}
+if (found)
+{
+pageHits++;
+}
+else
+{
+pageFaults++;
+frames[front] = pages[i];
+front = (front + 1) % f;
+}
+printf("%d\t", pages[i]);
+for (j = 0; j < f; j++)
+{
+if (frames[j] == -1)
+printf("- ");
+else
+printf("%d ", frames[j]);
+}
+if (found)
+printf("\tHit");
+else
+printf("\tPage Fault");
+printf("\n");
+}
+printf("\nTotal Page Faults = %d\n", pageFaults);
+printf("Total Page Hits = %d\n", pageHits);
+return 0;
+}
